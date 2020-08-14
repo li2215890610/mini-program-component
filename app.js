61dfd07f2424cb9c556d957eeced7a31a94b4f1a
yangDaXia <2215890610@qq.com>
2020-08-14 18:27:08 +0800

    增加微信版本更新

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
 App({
   onLaunch: function () {
     // 展示本地存储能力
-   
+
   },
   globalData: {
     userInfo: null
   },
-  onLaunch: function(qurey){
-    console.log(qurey,"onLaunchonLaunch");
+  onLaunch: function (qurey) {
+    console.log(qurey, "onLaunchonLaunch");
+    if (wx.canIUse("getUpdateManager")) {
+      const updateManager = wx.getUpdateManager();
+      updateManager.onCheckForUpdate(function (res) {
+        // 请求完新版本信息的回调
+      });
+      updateManager.onUpdateReady(function () {
+        wx.showModal({
+          title: "更新提示",
+          content: "新版本已经准备好，是否重启应用？",
+          success: function (res) {
+            if (res.confirm) {
+              // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+              updateManager.applyUpdate();
+            } else if (res.cancel) {
+              updateManager.applyUpdate();
+            }
+          }
+        });
+      });
+      updateManager.onUpdateFailed(function () {
+        // 新版本下载失败
+        console.log("onUpdateFailed");
+      });
+    } else {
+      wx.showModal({
+        title: "提示",
+        content: "您的微信版本过低，建议升级到最新版本。"
+      });
+    }
   },
   onShow: function (qurey) {
-    console.log(qurey,"onLoadonLoadonLoad");
+    console.log(qurey, "onLoadonLoadonLoad");
   },
   testShow: function (params) {
 
