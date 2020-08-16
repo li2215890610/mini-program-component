7a348a6a9520d1bbc04aa9b47a930f8f2bb0ddd9
yangDaXia <2215890610@qq.com>
2020-08-16 22:42:56 +0800

    修改步进器

--- components/vc-counter/vc-counter.js
+++ components/vc-counter/vc-counter.js
@@ -16,13 +16,13 @@ Component({
     // 这里放置自定义方法
     /* 点击减号 */
     bindMinus: function () {
-      var num = this.data.num;
+      let num = this.data.num;
       // 如果大于1时，才可以减 
       if (num > 1) {
         num--;
       }
       // 只有大于一件的时候，才能normal状态，否则disable状态 
-      var minusStatus = num <= 1 ? 'disabled' : 'normal';
+      let minusStatus = num <= 1 ? 'disabled' : 'normal';
       // 将数值与状态写回 
       this.setData({
         num: num,
@@ -31,11 +31,11 @@ Component({
     },
     /* 点击加号 */
     bindPlus: function () {
-      var num = this.data.num;
+      let num = this.data.num;
       // 不作过多考虑自增1 
       num++;
       // 只有大于一件的时候，才能normal状态，否则disable状态 
-      var minusStatus = num < 1 ? 'disabled' : 'normal';
+      let minusStatus = num < 1 ? 'disabled' : 'normal';
       // 将数值与状态写回 
       this.setData({
         num: num,
@@ -44,7 +44,7 @@ Component({
     },
     /* 输入框事件 */
     bindManual: function (e) {
-      var num = e.detail.value;
+      let num = e.detail.value;
       // 将数值与状态写回 
       this.setData({
         num: num
