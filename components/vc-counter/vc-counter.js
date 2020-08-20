069f87901b5b5b584641e171a5ae824c193334f6
yangDaXia <2215890610@qq.com>
2020-08-20 19:32:13 +0800

    格式化步进器

--- components/vc-counter/vc-counter.js
+++ components/vc-counter/vc-counter.js
@@ -6,7 +6,7 @@ Component({
     maxValue: {
       type: [Number, String],
       value: 10000,
-      observer: function(newVal){
+      observer: function (newVal) {
         this.setData({
           maxValue: parseInt(newVal, 10)
         })
@@ -15,7 +15,7 @@ Component({
     minValue: {
       type: [Number, String],
       value: 1,
-      observer: function(newVal){
+      observer: function (newVal) {
         this.setData({
           minValue: parseInt(newVal, 10)
         })
@@ -24,7 +24,7 @@ Component({
     value: {
       type: [Number, String],
       value: 1,
-      observer: function(newVal){
+      observer: function (newVal) {
         this.setData({
           value: parseInt(newVal, 10)
         })
@@ -33,43 +33,54 @@ Component({
   },
   methods: {
     reduce: function () {
-      let { value, minValue} = this.data;
+      let {
+        value,
+        minValue
+      } = this.data;
       // 如果大于 minValue 时，才可以减 
       if (value > minValue) {
         value--;
         // 将数值与状态写回 
         this.setData({
           value
-        },()=>{
-          this.triggerEvent('onChange',value)
+        }, () => {
+          this.triggerEvent('onChange', value)
         });
       }
     },
     add: function () {
-      let { value, maxValue} = this.data;
+      let {
+        value,
+        maxValue
+      } = this.data;
       // 不作过多考虑自增1 
       if (value !== maxValue) {
         value++;
         // 将数值与状态写回 
         this.setData({
           value
-        },()=>{          
-          this.triggerEvent('onChange',value)
+        }, () => {
+          this.triggerEvent('onChange', value)
         });
       }
     },
     onChange: function (e) {
-      const { maxValue, minValue } = this.data;
-      const { value} = e.detail;
+      const {
+        maxValue,
+        minValue
+      } = this.data;
+      const {
+        value
+      } = e.detail;
       const newValue = parseInt(value)
-      
-      if(newValue >= minValue && newValue <= maxValue){
+
+      if (newValue >= minValue && newValue <= maxValue) {
         this.setData({
           value: newValue
-        },()=>{
-          this.triggerEvent('onChange',newValue)
+        }, () => {
+          this.triggerEvent('onChange', newValue)
         });
-      }else{
+      } else {
         this.setData({
           value: maxValue
         })
